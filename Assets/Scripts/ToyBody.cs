ba82182a2c9f55a85b3367d202c93be00119b90f
Rodrigo Nietiedt <rodrigonietiedt@gmail.com>
2020-02-01 04:11:53 -0300

    prototyping

---
+++ Assets/Scripts/ToyBody.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyBody : MonoBehaviour
+{
+    static int LastId = 0;
+
+    public int Id{get; private set;}
+    public GameObject singleBodyRoot;
+    public GameObject fullBodyRoot;
+    public Transform[] singleBodyPosition;
+    public Transform[] fullBodyPositions;
+
+    private Transform[] currentBodyConfiguration;
+    public List<BodyPart> BodyParts;
+    // Update is called once per frame
+    public void Start()
+    {
+        Id = ++LastId;
+        UpdateBodyPartPositions();
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GetBodyParts();
+            UpdateBodyPartPositions();
+        }
+
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Split();
+        }
+    }
+
+    public void GetBodyParts()
+    {
+        
+    }
+    void UpdateBodyPartPositions()
+    {
+        singleBodyRoot.SetActive(false);
+        fullBodyRoot.SetActive(false);
+        switch(BodyParts.Count)
+        {
+            case 0:
+                Destroy(gameObject);
+                return;
+                break;
+            case 1:
+                currentBodyConfiguration = singleBodyPosition;
+                singleBodyRoot.SetActive(true);
+                break;
+            case 2:
+                currentBodyConfiguration = fullBodyPositions;
+                fullBodyRoot.SetActive(true);
+                break;
+
+        }
+        
+        for(int i = 0; i < BodyParts.Count; i++)
+        {
+            BodyParts[i].TargetPosition = currentBodyConfiguration[i];
+        }
+    }
+
+    public void Split()
+    {
+        if(BodyParts.Count <= 1)
+        {
+            return;
+        }
+
+        BodyPart removedPart = BodyParts[1];
+        BodyParts.RemoveAt(1);
+        ToyBodyFactory.Instance.InstantiateBody(removedPart, transform.position + (Vector3.up * 2), transform.rotation);
+        UpdateBodyPartPositions();
+    }
+
+    public void Join(ToyBody other)
+    {
+        if(BodyParts.Count > 1 || other.BodyParts.Count > 1)
+        {
+            return;
+        }
+        other.BodyParts.Add(BodyParts[0]);
+        other.UpdateBodyPartPositions();
+        DestroyThis();
+    }
+
+    public void DestroyThis()
+    {
+        ToyBodyFactory.Instance.ToyBodies.Remove(this);
+        Destroy(gameObject);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        ToyBody other = collision.gameObject.GetComponent<ToyBody>();
+        if(other == null) return;
+        if(other.Id < Id) return;
+
+        if(this.BodyParts.Count == 1 && other.BodyParts.Count == 1)
+        {
+            other.Join(this);
+        }
+        else
+        {
+            Split();
+            other.Split();
+        }
+    }
+}
---
+++ Assets/ToyBodyFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyBodyFactory : MonoBehaviour
+{
+    public List<ToyBody> ToyBodies;
+    public static ToyBodyFactory Instance;
+    public ToyBody Prefab;
+    public BodyPart[] BodyPartPrefabs;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            JoinTwo();
+        }
+    }
+
+    public void JoinTwo()
+    {
+        for(int i = ToyBodies.Count-2; i >= 0; i-=2)
+        {
+            Debug.Log($"{i} joins {i+1}");
+            ToyBodies[i+1].Join(ToyBodies[i]);
+        }
+    }
+
+    public ToyBody InstantiateBody(BodyPart bodyPart, Vector3 position, Quaternion rotation)
+    {
+        ToyBody toyBody = Instantiate<ToyBody>(Prefab, position, rotation);
+        toyBody.BodyParts = new List<BodyPart>();
+        toyBody.BodyParts.Add(bodyPart);
+        ToyBodies.Add(toyBody);
+        return toyBody;
+    }
+}
