9f5243d96f71a93925a28f801c6623f955e5a7c0
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2020-02-01 11:42:43 -0300

    Timer

---
+++ Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public float levelDuration;
+
+    private TimerSystem timerSystem;
+
+    private void Awake()
+    {
+        timerSystem = FindObjectOfType<TimerSystem>();
+
+        Events.Timer.TimerFinished += HandleFinishTimer;
+    }
+
+    private void OnDestroy()
+    {
+        Events.Timer.TimerFinished += HandleFinishTimer;
+    }
+
+    private void HandleFinishTimer()
+    {
+        Events.Level.LevelFinished.SafeInvoke();
+
+        Debug.Log("Level Finished");
+    }
+
+    void Start()
+    {
+        timerSystem.StartTimer(levelDuration);
+
+        Events.Level.LevelStarted.SafeInvoke();
+        
+        Debug.Log("Level Started");
+    }
+
+
+}
---
+++ Assets/Scripts/UI/TimerSlider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Awake()
+    {
+        Events.Timer.TimerStarted += Reset;
+        Events.Timer.TimerTick += Refresh;
+    }
+
+    private void OnDestroy()
+    {
+        Events.Timer.TimerStarted -= Reset;
+        Events.Timer.TimerTick -= Refresh;
+    }
+
+    private void Reset()
+    {
+        Refresh(1f);
+    }
+
+    private void Refresh(float value)
+    {
+        slider.value = value;
+    }
+}
