d8bdc0cab72ed6c5a42b133fcba88965258ac0bb
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2020-02-01 23:15:33 -0300

    global config and ring vfx on floating item

---
+++ Assets/Scripts/Systems/GlobalConfigs.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GlobalConfigs", fileName = "globalConfig")]
+public class GlobalConfigs : ScriptableObject
+{
+    public float toyTimeAfterJoinBeforeScore = 2f;
+}
