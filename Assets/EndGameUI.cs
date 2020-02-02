3700ae1635cb07873a3d075eb06b0545cbe34af4
Rodrigo Nietiedt <rodrigonietiedt@gmail.com>
2020-02-02 11:29:50 -0300

    wip

---
+++ Assets/EndGameUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameUI : MonoBehaviour
+{
+    public GameObject[] statePanels;
+    private void Start()
+    {
+        Reset();
+        Events.Level.Finish += OnLevelFinish;
+    }
+
+    private void OnDestroy()
+    {
+        Events.Level.Finish -= OnLevelFinish;
+    }
+
+    private void OnLevelFinish()
+    {
+        SetPanelsActive(true);
+    }
+
+    private void SetPanelsActive(bool active)
+    {
+        foreach(GameObject panel in statePanels)
+        {
+            panel.SetActive(active);
+        }
+    }
+
+    public void Reset()
+    {
+        SetPanelsActive(false);
+    }
+}
