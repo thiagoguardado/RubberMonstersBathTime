0c594491f0243983268a130278b9b5a819e1caf2
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2020-02-02 00:24:08 -0300

    mission create fix

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource bgm;
+    public AudioSource sfx;
+
+    
+}
